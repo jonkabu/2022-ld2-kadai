0bf2a155d7cd5ce6fa025594e3d0f0856105192b
jonkabu <jonkabu682@gmail.com>
2022-10-14 07:43:36 +0000

    課題１を追加

--- kadai-01/index.js
+++ kadai-01/index.js
@@ -1,2 +1,17 @@
 console.log("Hello World")
 console.log("bda,wdkh")
+
+const name = 'Tama'
+console.log(name)
+
+const firstName = 'Eisuke'
+const familyName =' Murakami'
+console.log(firstName + familyName)
+
+sum = 0;
+for(i=1; i<=10; i++){
+    sum += i;
+}
+console.log(sum)
+
+
