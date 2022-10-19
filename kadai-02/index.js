827dcd926810f04f1a0bea446415a2f985633380
jonkabu <jonkabu682@gmail.com>
2022-10-19 04:49:52 +0000

    課題2を追加

--- kadai-02/index.js
+++ kadai-02/index.js
@@ -1,1 +1,35 @@
-console.log('hello')
+// 変数 firstName と familyName を作成して自分の名前などを代入する
+const firstName = 'Eisuke'
+const familyName =' Murakami'
+
+// 文字列を結合する演算子を使って、フルネームを作成してを使って出力してみる 
+const fullName = firstName + familyName
+console.log(fullName)
+
+// 1から10までの和を出力
+const num = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(num)
+
+//sum = 0;
+//for(i=1; i<=10; i++){
+   // sum += i;
+//}
+//console.log(sum)
+
+//犬を飼っているかどうかの真偽値を格納する変数 dog と、猫を飼っているかどう
+//かの真偽値を格納する変数 cat を作成して値を格納する
+const dog = false
+const cat = true
+//「犬または猫を飼っている」場合の真偽値を計算して出力してみる
+//=> ヒント: OR 演算子(||) を利用する
+console.log("犬または猫を飼っている", dog || cat)
+
+//「犬と猫の両方を飼っている」場合の真偽値を計算して出力してみる
+//=> ヒント: AND 演算子(&&) を利用する
+console.log("犬と猫の両方を飼っている", dog && cat)
+
+//「犬も猫も飼っていない」場合の真偽値を計算して出力してみる
+//=> ヒント: 否定演算子(!)と、AND 演算子または OR 演算子を利用する
+console.log("犬も猫も飼っていない(「犬を飼っていない」かつ「猫を飼っていない」）", !dog && !cat)
+console.log("犬も猫も飼っていない（「犬または猫を飼っているわけではない」）", !(dog || cat))
+
---
+++ kadai-02/sample.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+const report = true
+const score = 80
+console.log(report && (score <= 60))
+
+const dog = true
+const cat = true
+
+
+
+
