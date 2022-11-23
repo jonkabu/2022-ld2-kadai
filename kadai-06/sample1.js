d38a6b9196f5288f5c9b04859795236238c05d7f
jonkabu <jonkabu682@gmail.com>
2022-11-23 05:29:46 +0000

    課題を追加

---
+++ kadai-06/sample1.js
@@ -0,0 +1,10 @@
+const a = {
+    "species": "cat",
+    "name": "Tama",
+    "age": 2,
+    "sex": "male"
+}
+    
+// JavaScript で JSON を扱う
+
+   const jsonstring = '{ "species": "cat", "name": "Tama" }'
